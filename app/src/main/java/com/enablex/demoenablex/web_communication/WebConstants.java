ef97177f5e52002ec7ff0620082c350f805130be
jaykumarvcx <jayprakash.k@vcloudx.com>
2021-12-09 13:33:25 +0530

    updated to sdk version 2.0.1 and reduce image size

--- app/src/main/java/com/enablex/demoenablex/web_communication/WebConstants.java
+++ app/src/main/java/com/enablex/demoenablex/web_communication/WebConstants.java
@@ -12,8 +12,8 @@ public class WebConstants {
     /*The following information required, Only when kTry = true, When you hosted your own webservice remove these fileds*/
 
     /*Use enablex portal to create your app and get these following credentials*/
-    public static final String kAppId = "App_ID";
-    public static final String kAppkey = "App_Key";
+    public static final String kAppId = "60ffb1c23494a67df74a7c78";
+    public static final String kAppkey = "ugyYahe8aQyVaAayetyuezyjuSuLypaPaYe2";
     public static final String getRoomId = "createRoom/";
     public static final int getRoomIdCode = 1;
     public static final String validateRoomId = "getRoom/";
